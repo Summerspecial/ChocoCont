68e6500380b795a3ef3ed1249bcac810ddf9bb4a
Summerspecial <traveller999@gmx.de>
2014-11-05 17:20:19 +0100

    pgp4usb chocolatey

---
+++ gpg4usb/0.3.3-1/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿$packageName = 'gpg4usb'
+$url = 'http://www.gpg4usb.org/gpg4usb-0.3.3-1.zip'
+$validExitCodes = @(0)Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
