3ea9b35a70877fe3c12c18ebdd0d786026f427a8
Summerspecial <Summerspecial@users.noreply.github.com>
2014-11-10 22:45:40 +0100

    Updated Natron 1.0.0 RC3

---
+++ Natron/1.0.0.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'Natron'
+$installerType = 'MSI'
+$url ='https://master-dl.sourceforge.net/project/natron/Natron_Windowsx86_32bits_v1.0.0-RC3.msi'
+$url64 ='https://master-dl.sourceforge.net/project/natron/Natron_Windows_x86_64bits_v1.0.0-RC3.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+
