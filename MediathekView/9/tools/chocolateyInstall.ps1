2a104480d55a2fb739aa6c59bb057ae84251eae9
Summerspecial <Summerspecial@users.noreply.github.com>
2015-03-15 01:23:16 +0100

    MediathekView Chocolatey initial

---
+++ MediathekView/9/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'MediathekView'
+$url = 'http://sourceforge.net/projects/zdfmediathk/files/Mediathek/Mediathek%209/MediathekView_9.zip/download'
+$validExitCodes = @(0)
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+try {
+  $target = Join-Path $MyInvocation.MyCommand.Definition "$($packageName)__Start.exe"
+  Install-ChocolateyDesktopLink $target
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
