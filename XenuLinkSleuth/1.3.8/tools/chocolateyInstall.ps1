5fdddf143e28339f45a5289d96751b0d5611a5c6
Summerspecial <Summerspecial@users.noreply.github.com>
2015-03-21 19:34:44 +0100

    Initial commit

---
+++ XenuLinkSleuth/1.3.8/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'XenuLinkSleuth'
+$installerType = 'EXE'
+$url = 'http://home.snafu.de/tilman/XENU.ZIP'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir"
+
+
+try { 
+  $FileFullPath = get-childitem $toolsDir -recurse -include *.exe | select -First 1  
+  Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$FileFullPath" -validExitCodes $validExitCodes
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
