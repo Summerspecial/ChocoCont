2cb013af80f117d42cf835b67867f6c90d99db6f
Summerspecial <Summerspecial@users.noreply.github.com>
2015-03-21 17:38:53 +0100

    Initial commit

---
+++ Antrenamer/2.10.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'Antrenamer'
+$installerType = 'EXE'
+$url = 'http://update.antp.be/renamer/antrenamer2_install.exe'
+$silentArgs = '/silent'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
